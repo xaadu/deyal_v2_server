1e09ddd49a3ac339deca7a94df8cafa8ce294135
xaadu <abdullahzayed01@gmail.com>
2023-09-01 19:46:17 +0600

    [ ADD ] therapist filter bny speciality id

--- therapists/api.py
+++ therapists/api.py
@@ -8,3 +8,10 @@ from .serializers import ThreapistSerializer
 class TherapistViewSet(viewsets.ModelViewSet):
     queryset = Therapist.objects.all().select_related("speciality")
     serializer_class = ThreapistSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        speciality = self.request.query_params.get("speciality_id")
+        if speciality:
+            qs = qs.filter(speciality_id=speciality)
+        return qs
