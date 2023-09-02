b9670fe5c54e988f0889dcd03f7eb4d324d05aeb
xaadu <abdullahzayed01@gmail.com>
2023-09-02 13:26:20 +0600

    [ UPDATE ] admin for SiteUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import SiteUser
 
-admin.site.register(SiteUser)
+
+@admin.register(SiteUser)
+class SiteUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "full_name",
+        "is_active",
+        "created_at",
+    )
+    search_fields = (
+        "email",
+        "full_name",
+    )
+    list_filter = ("is_active",)
+    ordering = ("is_active",)
