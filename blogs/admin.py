e76138329d86fa27f668a9221e98c1730d746cb2
xaadu <abdullahzayed01@gmail.com>
2023-09-02 13:39:54 +0600

    [ ADD ] blog admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -2,4 +2,27 @@ from django.contrib import admin
 
 from .models import Blog
 
-admin.site.register(Blog)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "title",
+        "therapist",
+        "is_active",
+    )
+    search_fields = (
+        "title",
+        "therapist__full_name",
+    )
+    list_filter = (
+        "is_active",
+        "therapist",
+    )
+    ordering = ("is_active",)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            qs = qs.filter(therapist_id=request.user.therapist.id)
+        return qs
