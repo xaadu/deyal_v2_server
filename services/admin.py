7abd67c0f5ba3720d8091d619628ad336d4d7bee
xaadu <abdullahzayed01@gmail.com>
2023-09-02 13:55:33 +0600

    [ UPDATE ] service admins

--- services/admin.py
+++ services/admin.py
@@ -5,5 +5,48 @@ from .models import (
     Symptom,
 )
 
-admin.site.register(Service)
-admin.site.register(Symptom)
+
+class SymptomInlineAdmin(admin.TabularInline):
+    model = Symptom
+    extra = 1
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "title",
+        "is_active",
+    )
+    search_fields = (
+        "title",
+        "description",
+        "what",
+        "cure",
+    )
+    inlines = (SymptomInlineAdmin,)
+    list_filter = ("is_active",)
+    ordering = ("is_active",)
+
+
+@admin.register(Symptom)
+class SymptomAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "service",
+        "get_details",
+        "is_active",
+    )
+    search_fields = (
+        "details",
+        "service__title",
+    )
+    list_filter = (
+        "is_active",
+        "service",
+    )
+    ordering = ("is_active",)
+
+    @admin.display(description="Details", ordering="details")
+    def get_details(self, obj):
+        return f"{obj.details[:30]}"
