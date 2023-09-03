c29b5f78b307d5ceb913a50de7c73019867aca69
xaadu <abdullahzayed01@gmail.com>
2023-09-03 07:27:53 +0600

    [ UPDATE ] posts admin

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,26 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "get_details",
+        "site_user",
+        "is_active",
+    )
+    search_fields = (
+        "details",
+        "site_user__email",
+    )
+    list_filter = (
+        "is_active",
+        "site_user",
+    )
+    ordering = (
+        "is_active",
+        "-id",
+    )
+
+    def get_details(self, instance):
+        return instance.details[:30]
